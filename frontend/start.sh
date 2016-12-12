ce0616c5a8b56927b58fc937a7d15065086e2aed
pdubois <pdub_3@yahoo.com>
2016-12-12 12:04:16 +0100

    Added API Explorer https://artifacts.alfresco.com/nexus/service/local/repositories/releases/content/org/alfresco/api-explorer/1.2/api-explorer-1.2.war

--- frontend/start.sh
+++ frontend/start.sh
@@ -3,5 +3,5 @@
 # param2: container name
 # param3: image name
 # To create the network: docker network create --driver bridge $1
-docker run --network=$1 -e INITIAL_PASS=admin -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro -d -i -t --name $2 $3
+docker run --network=$1  -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro   -d -i -t --name $2 $3
 
